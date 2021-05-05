c7f772852c1ea6f7f6c0ae99aa1ea7a08b3d9c7a
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-05-05 22:30:16 +0200

    feat: :sparkles: Add Abstract class Cereals that implements Item Interface

---
+++ src/main/java/org/pingpong/fastbreaktruck/domain/items/Cereals.java
@@ -0,0 +1,19 @@
+package org.pingpong.fastbreaktruck.domain.items;
+
+import org.pingpong.fastbreaktruck.domain.packing.Box;
+import org.pingpong.fastbreaktruck.domain.packing.Packing;
+
+public abstract class Cereals implements Item {
+
+    public Cereals() {}
+
+    @Override
+    public Packing packing() {
+        return new Box();
+    }
+
+    @Override
+    public String toString() {
+        return this.formatterToString();
+    }
+}
